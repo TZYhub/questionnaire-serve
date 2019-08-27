5da6f448ee575739f828efeed301049e10445f2d
tanzy <542382610@qq.com>
2019-08-27 13:15:41 +0800

    新增测试类

--- src/test/java/com/eric/service/TestIAnswerService.java
+++ src/test/java/com/eric/service/TestIAnswerService.java
@@ -1,15 +1,56 @@
 package com.eric.service;
 
 import com.eric.QuestionnaireServeApplication;
-import com.eric.service.interfaces.IUserService;
+import com.eric.entity.Answer;
+import com.eric.service.interfaces.IAnswerService;
+import com.eric.service.interfaces.IAnswerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = QuestionnaireServeApplication.class)
 public class TestIAnswerService {
+    @Autowired
+    private IAnswerService answerService;
 
+    @Test
+    public void testSaveAnswer() throws Exception{
+        Answer answer = new Answer();
+        answer.setQuestionType(1);
+        answer.setAnswerTime(new Date());
+        answer.setQuestionId(11111);
+        answer.setQuestionnaireId(10000001);
+        answer.setAnswerOption("[1,2]");
+        answerService.saveAnswer(answer);
+    }
+
+    @Test
+    public void testUpdateAnswer() throws Exception{
+        Answer answer = new Answer();
+        answer.setAnswerId(1);
+        answer.setQuestionType(2);
+        answerService.updateAnswer(answer);
+        System.out.println();
+    }
+
+    @Test
+    public void testDeleteAnswer() throws Exception{
+        System.out.println(answerService.deleteAnswer(1));
+    }
+
+    @Test
+    public void testSelectAnswerList() throws Exception{
+        Answer a = new Answer();
+        a.setAnswerId(1);
+        List<Answer> answers = answerService.queryAnswerList(a);
+        for(Answer answer: answers){
+            System.out.println(answer.getAnswerId());
+        }
+    }
 }
--- src/test/java/com/eric/service/TestIUserService.java
+++ src/test/java/com/eric/service/TestIUserService.java
@@ -1,12 +1,16 @@
 package com.eric.service;
 
 import com.eric.QuestionnaireServeApplication;
+import com.eric.entity.User;
 import com.eric.service.interfaces.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = QuestionnaireServeApplication.class)
@@ -15,7 +19,41 @@ public class TestIUserService {
     private IUserService userService;
 
     @Test
-    public void testInsertUser() throws Exception{
-
+    public void testSaveUser() throws Exception{
+        User u = new User();
+        //userId字段可自增
+        u.setUsername("tzyTest3");
+        u.setState(0);
+        u.setRandomCode("1234");
+        u.setEmail("1234@qq.com");
+        u.setCreateTime(new Date());
+        u.setLastLoginTime(new Date());
+        u.setPassword("1234");
+        userService.saveUser(u);
     }
+
+    @Test
+    public void testUpdateUser() throws Exception{
+        User u = new User();
+        u.setUserId(1000000001);//无主键时不更新任何数据，也不报错
+        u.setPassword("11111");
+        u.setUsername("tzyTest1");
+        System.out.println(userService.updateUser(u));
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception{
+        System.out.println(userService.deleteUser(1000000004));
+    }
+
+    @Test
+    public void testSelectUserList() throws Exception{
+        User u = new User();
+        u.setUserId(1000000001);
+        List<User> users = userService.queryUserList(u);
+        for(User user: users){
+            System.out.println(user.getUserId());
+        }
+    }
+
 }
