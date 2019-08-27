d7fa63bf23847fd650593c93522bdc4052119559
tanzy <542382610@qq.com>
2019-08-27 13:53:15 +0800

    添加查询user方法及测试json工具类

--- src/main/java/com/eric/controller/UserController.java
+++ src/main/java/com/eric/controller/UserController.java
@@ -2,6 +2,7 @@ package com.eric.controller;
 
 import com.eric.entity.User;
 import com.eric.service.interfaces.IUserService;
+import com.eric.utils.JSONResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 
 @RestController
@@ -32,6 +34,14 @@ public class UserController {
         user.setRandomCode("1234");
         log.debug("user = "+user);
         return userService.saveUser(user);
+    }
+
+    @RequestMapping("/queryUser")
+    public Object queryUser() throws Exception{
+        User user = new User();
+        user.setUserId(1000000005);
+        List<User> users = userService.queryUserList(user);
+        return JSONResultUtil.ok(users);
     }
 
 }
--- src/main/java/com/eric/utils/JSONUtil.java
+++
@@ -1,5 +0,0 @@
-package com.eric.utils;
-
-public class JSONUtil {
-
-}
